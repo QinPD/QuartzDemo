507339f6f716be33f410e9c8d4201a1a682f691d
QinPD <1321955819@qq.com>
2020-09-23 17:14:33 +0800

    添加DEMO

---
+++ WebJobTest/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Topshelf;
+
+namespace WebJobTest
+{
+    // To learn more about Microsoft Azure WebJobs SDK, please see https://go.microsoft.com/fwlink/?LinkID=320976
+    class Program
+    {
+        // Please set the following connection strings in app.config for this WebJob to run:
+        // AzureWebJobsDashboard and AzureWebJobsStorage
+        static void Main()
+        {
+            log4net.Config.XmlConfigurator.ConfigureAndWatch(new FileInfo(AppDomain.CurrentDomain.BaseDirectory + "log4net.config"));
+            HostFactory.Run(x =>
+            {
+                x.UseLog4Net();
+
+                x.Service<ServiceRunner>(s => {
+                    s.ConstructUsing(name => new ServiceRunner());
+                    s.WhenStarted((tc, hc) => tc.Start(hc));
+                    s.WhenStopped((tc, hc) => tc.Stop(hc));
+                    s.WhenContinued((tc, hc) => tc.Continue(hc));
+                    s.WhenPaused((tc, hc) => tc.Pause(hc));
+                });
+
+                x.RunAsLocalService();
+                x.StartAutomaticallyDelayed();
+
+                x.SetDescription("用于系统定时任务计划执行");
+                x.SetDisplayName("JobService");
+                x.SetServiceName("JobService");
+
+                x.EnablePauseAndContinue();
+            });
+        }
+    }
+}
---
+++ WebJobTest/TestJob.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Quartz;
+using log4net;
+
+namespace WebJobTest
+{
+    public class TestJob : IJob
+    {
+        private readonly ILog _logger = LogManager.GetLogger(typeof(TestJob));
+        public Task Execute(IJobExecutionContext context)
+        {
+            //_logger.Info("每3秒输出");
+            Console.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
+            return Task.FromResult(true);
+        }
+    }
+}
